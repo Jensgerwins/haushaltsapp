19322a27d0b361338c9c9b965d4afbd97a4792d2
Jens gerwins <Jens.gerwins@gmail.com>
2023-06-12 18:08:26 +0200

    s

--- script.js
+++ script.js
@@ -1,54 +1,58 @@
 const text = document.querySelector("#textFeld");
+const geldBtn = document.querySelector("#geldButton");
 const einBtn = document.querySelector("#btnEinkommen");
 const ulList = document.querySelector("#ulContainer");
 const ausBtn = document.querySelector("#btnAusgaben");
-const ulListAusgaben = document.querySelector("#ulContainerGesamt");
-let guthaben = 0;
-let ausgaben = 0;
-let gesamt = 0;
+const ulListguthaben = document.querySelector("#guthabeUl");
 
+let guthaben = [];
+let einkommen = [];
+let ausgaben = [];
+let geld = [];
+
+geldBtn.addEventListener("click", () => {
+
+
+})
 
 
 
 einBtn.addEventListener("click", () => {
+    const inputFeld = text.value;
+    listenErweiter(inputFeld)
+    parseInt(guthaben);
 
-    const textFeld = parseInt(text.value);
-    guthaben = textFeld;
-    ulList.innerHtml = "";
-    const newLi = document.createElement("li");
-    newLi.append("Einahmen:" + guthaben);
-    ulList.append(newLi);
-    gesamt = guthaben;
+
+
+
+
 
 })
 
 ausBtn.addEventListener("click", () => {
-    const textFeld = parseInt(text.value);
-    ausgaben = textFeld;
-    ulList.innerHtml = "";
-    const newLi = document.createElement("li");
-    newLi.append("Ausgaben:" + ausgaben);
-    ulList.append(newLi);
-    if (gesamt >= 0) {
-        const newLis = document.createElement("li");
-
-        ulList.innerHtml = "";
-        gesamt = guthaben - ausgaben;
-        newLis.append("Gesamt:" + gesamt);
-        ulListAusgaben.append(newLis);
-
-    }
-
-
 
 })
 
-function render() {
+
+
+
+
+function listenErweiter(array) {
+    ulList.innerText = "";
+    const gesamtList = document.createElement("li");
+    gesamtList.innerText = array;
+    inputFeldinnerHtml = "";
+    ulList.append(gesamtList);
 
 }
 
+function render(arr) {
+    const guthabLi = document.createElement("li");
+    guthabLi.innerText = arr;
+    ulListguthaben.append(arr);
+}
 
 
-
+render(guthaben);
 
 
